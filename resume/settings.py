a7506755d54445c23e361b4a83c21dfa464ded83
myselfajp <myjaliliparsa@gmail.com>
2021-11-02 22:03:48 +0300

    for heroku

--- resume/settings.py
+++ resume/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,8 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "statics",
 ]
 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
